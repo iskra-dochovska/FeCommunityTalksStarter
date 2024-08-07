718a045154cccccaf1ca78c9a855aa390012a064
Iskra Dochovska <iskra.dochovska@it-labs.com>
2024-08-07 14:24:32 +0200

    small addition

--- main.js
+++ main.js
@@ -15,3 +15,22 @@ const gameStartBtn = document.querySelector("#gameStartBtn")
 const gameEndDiv = document.querySelector("#gameEndDiv")
 const gameWinLoseSpan = document.querySelector("#gameWinLoseSpan")
 const gameEndScoreSpan = document.querySelector("#gameEndScoreSpan")
+
+//game configuration settings
+const config = {
+  type: Phaser.WEBGL, //a JavaScript API used for rendering interactive 2D and 3D graphics within web browsers without the need for plugins
+  width: sizes.width,
+  height: sizes.height,
+  canvas: gameCanvas,
+  physics: {
+    default: "arcade", //use the arcade physics engine
+    arcade: {
+      gravity: { y: speedDown },
+      debug: true,
+    },
+  },
+  scene: [GameScene],
+};
+
+//create the Phaser game instance with the specified configuration
+const game = new Phaser.Game(config);
